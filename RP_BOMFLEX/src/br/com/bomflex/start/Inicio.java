93c91a3db6b4c418f9e909db95c7ab4367eda638
pcFabrica <pcfabrica>
2014-05-30 10:42:32 -0300

    30-05-14  10:49

---
+++ RP_BOMFLEX/src/br/com/bomflex/start/Inicio.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.bomflex.start;
+
+/**
+ * @author Mailton Araújo - Analista de Sistemas
+ * Telefone - 71 - 3301-9000
+ * E-mail - mailton@sonomag.com.br
+ * 		    pcmotion@gmail.com
+ */
+public class Inicio {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
