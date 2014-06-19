cede89612b96304c05c4e0b0136cf0087d963f68
Mailton <Mailton>
2014-06-19 11:28:20 -0300

    19/06/2014      11:28

---
+++ RP_BOMFLEX/src/br/com/bomflex/model/dao/conexao/ConexaoLocal.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package br.com.bomflex.model.dao.conexao;
+
+/**
+ * @author Mailton Araújo - Analista de Sistemas
+ * Telefone - 71 - 3301-9000
+ * E-mail - mailton@sonomag.com.br
+ * 		    pcmotion@gmail.com
+ */
+public class ConexaoLocal {
+
+}
--- RP_BOMFLEX/src/br/com/bomflex/testes/Teste.java
+++ RP_BOMFLEX/src/br/com/bomflex/testes/Teste.java
@@ -1,20 +1,35 @@
 package br.com.bomflex.testes;
 
-import br.com.bomflex.controller.erro.StringErros;
-import br.com.bomflex.controller.validacao.Validacao;
 
-public class Teste {
-
+public class Teste extends Thread{
+	long numero=0;
+	long tempo;
+	String nomeClasse;
+	
+	public Teste(long tempo,String nomeClasse){
+		this.nomeClasse = nomeClasse;
+		this.tempo = tempo;
+	}
+	
+	public void run (){
+		try {
+		for(int i=0; i < 3; i++){
+			System.out.println(nomeClasse+" - "+numero);
+			numero++;
+			Thread.sleep(tempo);
+		}
+		} catch (InterruptedException e) {
+			return;
+		}
+	}
+	
 	public static void main(String[] args) {
-		Validacao valida = new Validacao();
-		System.out.println("CEP - "+ valida.validaCEP("41330000"));
-		System.out.println("RG - "+ valida.validaRG("1144132231"));
-		System.out.println("CNPJ - "+ valida.validaCNPJ("02735358000198"));
-		System.out.println("TELEFONE - "+ valida.validaTelefone("071", "33019000"));
-		System.out.println("NOME - "+ valida.validaNome("Mailton", 3, 20));
-		System.out.println("CPF - "+ valida.validaCPF("03511971500"));
-		StringErros teste = new StringErros();
-		teste.exibeErroTela(teste.getErros("2"));
+		
+		new Teste(1000,"Teste 1").start();
+		new Teste(2000,"Teste 2").start();
+		new Teste(3000,"Teste 3").start();
+		new Teste(4000,"Teste 4").start();
+		
 	}
 
 }
