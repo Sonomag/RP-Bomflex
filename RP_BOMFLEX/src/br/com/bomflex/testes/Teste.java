3581fbf42eb1f75f2ac494f13c289751f65e3e21
Mailton <Mailton>
2014-07-27 23:43:49 -0300

    27/07/2014

--- RP_BOMFLEX/src/br/com/bomflex/testes/Teste.java
+++ RP_BOMFLEX/src/br/com/bomflex/testes/Teste.java
@@ -1,34 +1,37 @@
 package br.com.bomflex.testes;
 
-
 public class Teste extends Thread{
 	long numero=0;
 	long tempo;
 	String nomeClasse;
+	long tempoInicial;
 	
-	public Teste(long tempo,String nomeClasse){
+	public Teste(long tempo,String nomeClasse,long tempoInicial){
 		this.nomeClasse = nomeClasse;
 		this.tempo = tempo;
+		this.tempoInicial = tempoInicial;
 	}
 	
 	public void run (){
 		try {
-		for(int i=0; i < 3; i++){
-			System.out.println(nomeClasse+" - "+numero);
-			numero++;
-			Thread.sleep(tempo);
-		}
+			int y=0;
+			for(int i=0; i < tempoInicial; i++){
+				numero++;
+				tempo = (System.currentTimeMillis()-tempoInicial);
+				System.out.println(" CONTADOR "+y+" "+tempo);
+				Thread.sleep(tempo);
+				y++;
+			}
 		} catch (InterruptedException e) {
 			return;
 		}
 	}
-	
 	public static void main(String[] args) {
 		
-		new Teste(1000,"Teste 1").start();
-		new Teste(2000,"Teste 2").start();
-		new Teste(3000,"Teste 3").start();
-		new Teste(4000,"Teste 4").start();
+		new Teste(1000,"Teste 1",System.currentTimeMillis()).start();
+//		new Teste(2000,"Teste 2",System.currentTimeMillis()).start();
+//		new Teste(3000,"Teste 3",System.currentTimeMillis()).start();
+//		new Teste(4000,"Teste 4",System.currentTimeMillis()).start();
 		
 	}
 
